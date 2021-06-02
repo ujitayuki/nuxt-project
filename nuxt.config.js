6b51644780795b8b83dbbd212b1a983178782afe
Yuki Ujita <ujita.yuki0329@gmail.com>
2021-06-02 17:15:52 +0900

    first deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,6 +29,7 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     { src: '~/plugins/main.js', ssr: false},
+     '~/plugins/firebase'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
---
+++ pages/sample.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <button @click="submit">送信</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    submit() {
+      const db = this.$firebase.firestore();
+      db.collection('user').add({ name: 'hoge' })
+        .then(() => {
+          console.log('成功');
+        })
+        .catch((e) => {
+          console.log(e.message);
+        });
+    }
+  }
+}
+</script>
